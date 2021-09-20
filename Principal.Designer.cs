568278883a5044dcf0358b02b548969eb7d707c0
Rolyn Oswaldo Hernandez Gamez <rolyn_hernandez2003@hotmail.com>
2021-09-20 10:57:49 -0600

    Boton Saludo

--- Principal.Designer.cs
+++ Principal.Designer.cs
@@ -29,7 +29,17 @@ namespace SCCI
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hola 12 BTPI";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Principal
             // 
@@ -37,6 +47,7 @@ namespace SCCI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Principal";
             this.Text = "Principal";
             this.ResumeLayout(false);
@@ -44,6 +55,8 @@ namespace SCCI
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
